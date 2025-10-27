b074610d73a7f459bf849373c1486e54c6470fad
Matej Anchev <matej.anchev@students.finki.ukim.mk>
2025-10-27 22:21:16 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/lab/VnpLab1Application.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnpLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnpLab1Application.class, args);
+	}
+
+}
